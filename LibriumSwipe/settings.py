f244afd6ac17830da66c42844ef94c93f05c26ec
Ricardo <ricardo.csantos0877@gmail.com>
2021-07-29 22:53:27 -0300

    feat: começando pág inicial

--- LibriumSwipe/settings.py
+++ LibriumSwipe/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'index',
+    'autenticacao',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ autenticacao/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AutenticaçãoConfig(AppConfig):
+    name = 'autenticação'
