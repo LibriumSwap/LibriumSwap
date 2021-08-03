fc2ce75ea87b79444cad45bbdbb748a7c8a5ebb0
Ricardo <ricardo.csantos0877@gmail.com>
2021-08-02 23:39:27 -0300

    feat: esqueci senha completo

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    path('reset-password', views.password_reset_request, name='password_reset'), 
+    path('reset-password/done', PasswordResetDoneView.as_view(), name='password_reset_done'), 
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'), 
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
 from django.db import IntegrityError
 from django.urls import reverse
+from django.core.mail import send_mail, BadHeaderError
+from django.contrib.sites.shortcuts import get_current_site
+
 
 from .forms import LoginForm, RegisterForm
 from .models import CustomUser as User
@@ -61,3 +71,31 @@ def register(request):
 		return render(request, "autenticacao/register.html", {
 			"form": RegisterForm()
 			})
+
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			user = User.objects.get(Q(email=data))
+			current_site = get_current_site(request)
+			if user:
+				subject = "Solicitação de troca de senha"
+				email_template_name = "registration/password_reset_email.txt"
+				c = {
+				"email":user.email,
+				'domain': current_site.domain,
+				'site_name': current_site.name,
+				"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+				"user": user,
+				'token': default_token_generator.make_token(user),
+				'protocol': 'http',
+				}
+				email = render_to_string(email_template_name, c)
+				try:
+					send_mail(subject, email, 'admin@libriumswap.live' , [user.email], fail_silently=False)
+				except BadHeaderError:
+					return HttpResponse('Invalid header found.')
+				return redirect ("/reset-password/done")
+			return HttpResponseRedirect(reverse('index'))
+	return render(request, "registration/password_reset_form.html", context={"form":password_reset_form})
