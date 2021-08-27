54a21df970bd0c9023124ee41fd73bf0e1efc95f
Ricardo <ricardo.csantos0877@gmail.com>
2021-08-27 16:56:09 -0300

    feat: adicionar anuncio ao carrinho

--- carrinho/urls.py
+++ carrinho/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.carrinho, name="carrinho")
+	path("", views.carrinho, name="carrinho"),
+	path("adicionar/", views.adicionar_ao_carrinho, name="adicionar_ao_carrinho")
 ]
--- carrinho/views.py
+++ carrinho/views.py
@@ -1,4 +1,30 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
+from livros.models import LivroAnuncio
 
 def carrinho(request):
-	return render(request, "carrinho.html", {})
+	print(request.session.get('carrinho'))
+	anuncios = []
+	if request.session.get('carrinho'):
+		for anuncio_id in request.session['carrinho']:
+			anuncio = get_object_or_404(LivroAnuncio, id=anuncio_id)
+			anuncios.append(anuncio)
+
+	return render(request, "carrinho.html", {
+		"carrinho": anuncios
+		})
+
+def adicionar_ao_carrinho(request):
+	data = json.loads(request.body)
+	anuncio_id = data.get('anuncio_id')
+
+	if not request.session.get('carrinho'):
+		request.session['carrinho'] = []
+
+	request.session['carrinho'].append(anuncio_id)
+	request.session.modified = True
+
+	return JsonResponse({"success": "adicionado"})
