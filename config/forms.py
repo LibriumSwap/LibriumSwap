ce70f772a2f320ff09b2d5caa78043d3161846a1
dudujunior12 <dudu12310@hotmail.com>
2021-11-11 16:16:41 -0300

    config templates update, and added image form

--- config/forms.py
+++ config/forms.py
@@ -1,2 +1,8 @@
 from autenticacao.models import User
 from django import forms
+
+class ConfigForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields = ['profile_image']
+		exclude = ['email', 'is_funcionario', 'favoritos']
