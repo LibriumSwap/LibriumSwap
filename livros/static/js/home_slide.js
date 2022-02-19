0503537b60e214bd6ffee18d49df565f0686cb02
Ricardo <ricardo.csantos0877@gmail.com>
2022-02-19 17:19:22 -0300

    feat: responsive homepage

--- livros/static/js/home_slide.js
+++ livros/static/js/home_slide.js
@@ -39,3 +39,45 @@ const swiper = new Swiper('.swiper', {
     }
   }
 });
+
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  spaceBetween: 10,
+  slidesPerView: 3,
+  autoHeight: true,
+  freeMode: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    416: {
+      slidesPerView: 4
+    },
+    518: {
+      slidesPerView: 5,
+    },
+    520: {
+      slidesPerView: 3,
+    },
+    690: {
+      slidesPerView: 4,
+    },
+    876: {
+      slidesPerView: 5,
+    },
+    1052: {
+      slidesPerView: 4,
+    },
+    1317: {
+      slidesPerView: 5,
+    },
+    1610: {
+      slidesPerView: 6,
+    }
+  }
+});
