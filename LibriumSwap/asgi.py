0c6a69a448fc9dbf85ae9b69222dd204441c2092
dudujunior12 <dudu12310@hotmail.com>
2022-03-01 07:01:54 +0000

    fix: asgi.py

--- LibriumSwap/asgi.py
+++ LibriumSwap/asgi.py
@@ -8,13 +8,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibriumSwap.settings')
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibriumSwipe.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
