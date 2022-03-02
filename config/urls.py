7d1335f67954c87aeb8ef978f6dbf4c56b0150fe
dudujunior12 <dudu12310@hotmail.com>
2022-03-02 10:41:27 -0300

    create: add new payment page and css

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('settings/', views.settingsView, name="settings"),
-    path('settings/privacity', views.privacityView, name="privacity"),
-    path('settings/security', views.securityView, name="security"),
-    path('settings/profile', views.profileView, name="profile"),
-    path('settings/payment', views.paymentView, name="payment"),
-    path('settings/notifications', views.notificationsView, name="notifications"),
+    path('settings/my-account/privacity', views.privacityView, name="privacity"),
+    path('settings/my-account/security', views.securityView, name="security"),
+    path('settings/my-account/profile', views.profileView, name="profile"),
+    path('settings/my-account/payment', views.paymentView, name="payment"),
+    path('settings/my-account/notifications', views.notificationsView, name="notifications"),
+    path('settings/my-account/payment/new-payment/', views.newPaymentView, name="new-payment"),
 ]
