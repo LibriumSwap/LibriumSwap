8f742e8d3bdd1c9439307a6b615f7e7aa98a31da
Ricardo <ricardo.csantos0877@gmail.com>
2022-03-09 00:21:19 -0300

    fix: author images

--- index/views.py
+++ index/views.py
@@ -1,10 +1,9 @@
 import requests
 import json
+import wikipedia
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.db.models import Count
-
-
 from livros.models import LivroAnuncio
 
 
@@ -19,27 +18,27 @@ def home(request):
 	autores_populares = LivroAnuncio.objects.values("autor").order_by("autor").annotate(the_count=Count("autor"))
 	autores_populares = autores_populares.order_by("-the_count")[:10]
 	for autor in autores_populares:
-		autor["imagem"] = get_wiki_main_image(autor["autor"])
+		autor["imagem"] = "/static/images/autor_not_found.png"
 
 	return render(request, "home/home.html", {
 		"recentes": recentes,
 		"autores_populares": autores_populares
 		})
 
-def get_wiki_main_image(title):
-    url = 'https://en.wikipedia.org/w/api.php'
-    data = {
-        'action' :'query',
-        'format' : 'json',
-        'formatversion' : 2,
-        'prop' : 'pageimages|pageterms',
-        'piprop' : 'original',
-        'titles' : title
-    }
-    response = requests.get(url, data)
-    json_data = json.loads(response.text)
-    
-    if json_data['query']['pages'][0].get('original'):
-    	return json_data['query']['pages'][0]['original']['source']
-    else:
-    	return "/static/images/autor_not_found.png"
+
+"""
+WIKI_REQUEST = 'http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='
+
+def get_wiki_image(search_term):
+    try:
+        result = wikipedia.search(search_term, results = 1)
+        wikipedia.set_lang('en')
+        wkpage = wikipedia.WikipediaPage(title = result[0])
+        title = wkpage.title
+        response  = requests.get(WIKI_REQUEST+title)
+        json_data = json.loads(response.text)
+        img_link = list(json_data['query']['pages'].values())[0]['original']['source']
+        return img_link        
+    except:
+        return "/static/images/autor_not_found.png"
+"""
