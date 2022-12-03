afffca6a6fe2e5e97874c6279fcf52a64a42bbc8
dudujunior12 <dudu12310@hotmail.com>
2022-12-03 18:01:08 -0300

    update: settings.py redis

--- LibriumSwap/settings.py
+++ LibriumSwap/settings.py
@@ -92,7 +92,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('0.0.0.0', 6379)],
+            "hosts": [('0.0.0.0', 6379), env("REDISURL")],
         },
     },
 }
